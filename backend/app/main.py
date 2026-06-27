6be0da27b6fca31a256fc86fca03e39e626898ef
Parth Popli <parthpopli17@gmail.com>
2026-06-27 19:04:14 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.analyze import router as analyze_router
 from app.api.ask import router as ask_router
 
@@ -7,16 +8,19 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://main.dzt9gg3cjbgm.amplifyapp.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-def home():
-    return {"message": "RepoLens Backend Running"}
-
 app.include_router(analyze_router)
 app.include_router(ask_router)
 
+
+@app.get("/")
+def home():
+    return {"message": "RepoLens Backend Running"}
